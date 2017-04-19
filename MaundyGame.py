6822dc1e821ce5b231aa4ec3832f42f3a8ae977b
Blakeley Hoffman <blakeleyhoffman@gmail.com>
2017-04-19 18:15:32 -0400

    changes from tuesday

--- MaundyGame.py
+++ MaundyGame.py
@@ -7,8 +7,17 @@ class AIPlayer(object):
 	# 	number = findSmallestFactor
 	# 	game.cut(board, number, )
 
-	def findSmallestFactor(height):
-		h = math.sqrt(height)
+	def findAIMove(height):
+		if(isPrime(height)):
+			return height
+
+		for i in range(height, 2, -1):
+			if(isPrime(i) and height%i == 0):
+				return i
+
+	def isPrime(number):
+
+		h = math.ceil(math.sqrt(number))
 
 		for i in xrange(2,h):
 			if height%i == 0:
@@ -49,20 +58,65 @@ class MaundyGame(object):
 
     def represent(self):
  		for i in xrange(0,len(self.board)):
+ 			print "Board " + i + ": "
  			for j in xrange(0, self.board[i][1]):
  				for k in xrange(0, self.board[i][0]):
  					print "x",
  				print "\n",
  			print "\n"
 
+    def checkFinish(self, isLeft):
+    	if(isLeft): 
+    		for i in xrange(0, len(self.board)):
+    			if self.board[i][0] > 1:
+    				return False
+    		return True
+    	else:
+    		for i in xrange(0, len(self.board)):
+    			if self.board[i][1] > 1: 
+    				return False
+    		return True
 
 
 if __name__ == '__main__':
 	 
-	game = MaundyGame(6,9)
 	game.represent()
 	game.cut(0,3,False)
 	game.represent()
+
+	print "Welcome to Maundy Cake!"
+	width = int(raw_input('Please enter a width for the board: '))
+	height = int(raw_input('Please enter a height for the board: '))
+
+	print "The width for your board is " + width + "and the height for your board is " + height + "."
+
+	game = MaundyGame(width,height)
+
+	if(game.leftCurrent):
+		print "Your move!"
+	else:
+		print "The computer will move first."
+
+
+	while(not(game.checkFinish)):
+		if(game.leftCurrent):
+
+			game.represent()
+
+			boardNum = int(raw_input('Which board would you like to cut?'))
+			while(boardNum >= len(self.board)):
+				boardNum = int(raw_input('Oh no! Please try again. Which board would you like to cut?'))
+
+			cutNum = int(raw_input('How many pieces would you like to cut the board into?'))
+			while(self.board[boardNum][0]%cutNum != 0 or cutNum == 1):
+				cutNum = int(raw_input('Oh no! Please try again. How many pieces would you like to cut the board into?'))
+
+			game.cut(boardNum, cutNum, True)
+
+		else:
+
+
+		game.leftCurrent = not(game.leftCurrent)
 
 
 
