a6f382d0afcbf7857d99ff3db8ab16152296a761
Blakeley Hoffman <blakeleyhoffman@gmail.com>
2017-04-19 18:56:44 -0400

    updated code from tuesday -we're operational!

--- MaundyGame.py
+++ MaundyGame.py
@@ -1,28 +1,23 @@
 import random as rand
 import math as math
 
-class AIPlayer(object):
 
-	# def move(game):
-	# 	number = findSmallestFactor
-	# 	game.cut(board, number, )
-
-	def findAIMove(height):
-		if(isPrime(height)):
-			return height
-
-		for i in range(height, 2, -1):
-			if(isPrime(i) and height%i == 0):
-				return i
-
-	def isPrime(number):
-
-		h = math.ceil(math.sqrt(number))
-
-		for i in xrange(2,h):
-			if height%i == 0:
-				return i
+def findAIMove(height):
+	if(isPrime(height)):
 		return height
+
+	for i in range(height, 2, -1):
+		if(isPrime(i) and height%i == 0):
+			return i
+
+def isPrime(number):
+
+	h = math.ceil(math.sqrt(number))
+
+	for i in xrange(2,h):
+		if height%i == 0:
+			return i
+	return height
 
 
 class MaundyGame(object):
@@ -49,7 +44,7 @@ class MaundyGame(object):
         	self.board = self.board[:whichBoard] + [(self.board[whichBoard][0] / number, self.board[whichBoard][1])]
         	print "board", self.board
         	for i in xrange(1, number):
-        		print "self", self.board[whichBoard]
+        		#print "self", self.board[whichBoard]
         		self.board.append(self.board[whichBoard])
         else:
         	self.board = self.board[:whichBoard] + [(self.board[whichBoard][0], self.board[whichBoard][1] / number)]
@@ -58,7 +53,7 @@ class MaundyGame(object):
 
     def represent(self):
  		for i in xrange(0,len(self.board)):
- 			print "Board " + i + ": "
+ 			print "Board " + str(i) + ": "
  			for j in xrange(0, self.board[i][1]):
  				for k in xrange(0, self.board[i][0]):
  					print "x",
@@ -70,25 +65,27 @@ class MaundyGame(object):
     		for i in xrange(0, len(self.board)):
     			if self.board[i][0] > 1:
     				return False
+    		print "The game is finished! You lost!"
     		return True
     	else:
     		for i in xrange(0, len(self.board)):
     			if self.board[i][1] > 1: 
     				return False
+    		print "The game is finished! You won!"
     		return True
 
 
 if __name__ == '__main__':
 	 
-	game.represent()
-	game.cut(0,3,False)
-	game.represent()
+	# game.represent()
+	# game.cut(0,3,False)
+	# game.represent()
 
 	print "Welcome to Maundy Cake!"
 	width = int(raw_input('Please enter a width for the board: '))
 	height = int(raw_input('Please enter a height for the board: '))
 
-	print "The width for your board is " + width + "and the height for your board is " + height + "."
+	print "The width for your board is " + str(width) + " and the height for your board is " + str(height) + "."
 
 	game = MaundyGame(width,height)
 
@@ -97,23 +94,24 @@ if __name__ == '__main__':
 	else:
 		print "The computer will move first."
 
+	while(not(game.checkFinish(game.leftCurrent))):
 
-	while(not(game.checkFinish)):
+		game.represent()
+
 		if(game.leftCurrent):
 
-			game.represent()
-
 			boardNum = int(raw_input('Which board would you like to cut?'))
-			while(boardNum >= len(self.board)):
+			while(boardNum >= len(game.board)):
 				boardNum = int(raw_input('Oh no! Please try again. Which board would you like to cut?'))
 
 			cutNum = int(raw_input('How many pieces would you like to cut the board into?'))
-			while(self.board[boardNum][0]%cutNum != 0 or cutNum == 1):
+			while(game.board[boardNum][0]%cutNum != 0 or cutNum == 1):
 				cutNum = int(raw_input('Oh no! Please try again. How many pieces would you like to cut the board into?'))
 
 			game.cut(boardNum, cutNum, True)
 
 		else:
+			print "something"
 
 
 		game.leftCurrent = not(game.leftCurrent)
